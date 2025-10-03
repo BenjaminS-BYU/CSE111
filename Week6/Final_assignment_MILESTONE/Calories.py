476f49fba660f847b8f1905dcd137974aa29fca7
BenjaminS-BYU <bstrong2@byupathway.edu>
2025-10-03 17:25:56 -0600

    added some stuff to the main file

---
+++ Week6/Final_assignment_MILESTONE/Calories.py
@@ -0,0 +1,80 @@
+import datetime
+import csv
+import math
+"""This program is aimed at helping people track their calorie intake from a list of foods and new foods they add. This program will let you add food, remove food, look at a quote for motivation 
+and will calculate the total caloric intake for that day."""
+
+FOOD_DICT = "common_foods_with_mealplan.csv"
+DAILY_FOOD = "daily_food_calories.txt"
+FOOD_NAME_INDEX = 0
+FOOD_CAL_PER_100_INDEX = 1
+food_dict = {}
+
+def main():
+    food_dict_csv = food_dict_maker(FOOD_DICT)
+
+    quote: str = get_quote()
+    print("="*(len(quote)))
+    print(quote)
+    print("="*(len(quote)))
+
+    while True:
+        print("""Welcome to the calorie tracking program. Please pick from the following:
+
+Menu:
+1. Add Food
+2. Remove Food
+3. Your Food
+4. Change Quote
+5. Exit
+    """)
+        users_choice = input("Your choice: ")
+        if users_choice == "5":
+            print("Have a great day!")
+            exit()
+        elif users_choice == "1":
+            food = input("What food would you like to add? ")
+            add_food(food,food_dict_csv)
+        elif users_choice == "2":
+            pass
+        elif users_choice == "3":
+            pass
+        elif users_choice == "4":
+            pass
+        else:
+            print("Input not valid, please try again")
+            continue
+    
+def food_dict_maker(csv_file):
+    """Takes in the list given from the csv file and returns a dictionary"""
+    pass
+
+def add_food(food, food_file):
+    """This function will allow the user to add a food to the 
+list on a txt file"""
+    pass
+
+def remove_food(food, food_file):
+    """This function takes the food prama and take it out of 
+the list txt file from the days list"""
+    pass
+
+def show_food(food_file):
+    """This shows the list txt that holds the food added"""
+    pass
+
+def get_quote() -> str:
+    """goes into the quote txt and picks a random quote to show"""
+    return "You got this!"
+
+def format_input(user_input):
+    """This function takes the users input like '  Apple  ' 
+and standardizes it to to strip it and lower case it and 
+returns the outcome 'apple'"""
+    pass
+
+
+
+# For testing purposes don't call main when testing
+if __name__ == "__main__":
+    main()
