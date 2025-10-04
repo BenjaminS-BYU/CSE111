4910a99d6f41239bcf8700c1537994480de9b244
Baggins3345 <baggins3345@gmail.com>
2025-10-04 12:31:36 -0600

    Fixed the quote reader to encoding utf-8

--- Week6/Final_assignment_MILESTONE/Calories.py
+++ Week6/Final_assignment_MILESTONE/Calories.py
@@ -1,6 +1,7 @@
 import datetime
 import csv
 import math
+import random
 """This program is aimed at helping people track their calorie intake from a list of foods and new foods they add. This program will let you add food, remove food, look at a quote for motivation 
 and will calculate the total caloric intake for that day."""
 
@@ -10,17 +11,19 @@ QUOTES_TXT = "motivational_weight_loss_quotes.txt"
 FOOD_NAME_INDEX = 0
 FOOD_CAL_PER_100_INDEX = 1
 food_dict = {}
+quote_dict = {}
 
 def main():
     food_dict_csv = food_dict_maker(FOOD_DICT)
-    quotes_list = motivation_dict_maker(QUOTES_TXT)
+    quotes_dict = motivation_dict_maker(QUOTES_TXT)
 
-    quote: str = get_quote(quotes_list)
-    print("="*(len(quote)))
-    print(quote)
-    print("="*(len(quote)))
+    quote= get_quote(quotes_dict)
+    
 
     while True:
+        print("="*(len(quote)))
+        print(quote)
+        print("="*(len(quote)))
         print("""Welcome to the calorie tracking program. Please pick from the following:
 
 Menu:
@@ -42,21 +45,24 @@ Menu:
         elif users_choice == "3":
             pass
         elif users_choice == "4":
-            pass
+            quote= get_quote(quotes_dict)
+            continue
         else:
             print("Input not valid, please try again")
             continue
 
 def motivation_dict_maker(txt_file):
-    with open(txt_file) as file:
+    """This function takes a txt file such as the quotes and turns it into a dict with the numbers as keys and 
+    quotes as values"""
+    with open(txt_file, encoding="utf-8") as file:
         next(file)
 
         for line in file:
-            num = line[0]
-            quote = line[1]
-
-
-
+            seperate_line = line.split(".")
+            key = seperate_line[0]
+            quote = seperate_line[1]
+            quote_dict[key] = quote
+    return quote_dict
 
 def food_dict_maker(csv_file):
     """Takes in the list given from the csv file and returns a dictionary"""
@@ -87,9 +93,11 @@ def show_food(food_file):
     """This shows the list txt that holds the food added"""
     pass
 
-def get_quote(quotes_list) -> str:
+def get_quote(quotes):
     """goes into the quote txt and picks a random quote to show"""
-    return "You got this!"
+    num = str(random.randint(1,102))
+    return f"{quotes[num]}!"
+        
 
 def format_input(user_input):
     """This function takes the users input like '  Apple  ' 
