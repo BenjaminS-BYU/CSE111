c408d97156632724d28e8ab534c900bedf46fe7b
Baggins3345 <baggins3345@gmail.com>
2025-10-06 10:34:10 -0600

    got the items to be written to the empty list and the calories

--- Week6/Final_assignment_MILESTONE/Calories.py
+++ Week6/Final_assignment_MILESTONE/Calories.py
@@ -88,13 +88,18 @@ def food_dict_maker(csv_file):
 def add_food(food, food_file):
     """This function will allow the user to add a food to the 
 list on a txt file"""
+
+    if food in food_dict:
+        calories = food_dict[food]
+    else:
+        calories = 0
     num_lines = 0
     with open(food_file, "r") as daily_food_read:
         num_lines = sum(1 for _ in daily_food_read) + 1
     with open(food_file, "a") as daily_food:
-        daily_food.write(f"{num_lines}: {food}\n")
+        daily_food.write(f"{num_lines}, {food}, {calories}\n")
     return f"{food} was add to the daily list."
-            
+
 
 def remove_food(food, food_file):
     """This function takes the food prama and take it out of 
