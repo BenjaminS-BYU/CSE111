eb3978b6a1789d2c9b522e5984f60ba46bc59706
Baggins3345 <baggins3345@gmail.com>
2025-10-06 11:16:48 -0600

    got the date functino working and changed the file to a csv file

--- Week6/Final_assignment_MILESTONE/Calories.py
+++ Week6/Final_assignment_MILESTONE/Calories.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime
 import csv
 import random
 """This program is aimed at helping people track their calorie intake from a list of foods and new foods they add. This program will let you add food, remove food, look at a quote for motivation 
@@ -7,7 +7,7 @@ date is diffrent."""
 
 # Consts
 FOOD_DICT = "common_foods_with_mealplan.csv"
-DAILY_FOOD = "daily_food_calories.txt"
+DAILY_FOOD = "daily_food_calories.csv"
 QUOTES_TXT = "motivational_weight_loss_quotes.txt"
 KEY_INDEX = 0
 VALUE_INDEX = 1
@@ -17,8 +17,9 @@ food_dict = {}
 quote_dict = {}
 
 def main():
-    food_dict_csv = food_dict_maker(FOOD_DICT)
+    food_dict_maker(FOOD_DICT)
     quotes_dict = motivation_dict_maker(QUOTES_TXT)
+    todays_date(DAILY_FOOD)
 
     quote= get_quote(quotes_dict)
     
@@ -30,7 +31,7 @@ def main():
         print("""Welcome to the calorie tracking program. Please pick from the following:
 
 Menu:
-1. Add Food (Not working yet)
+1. Add Food (Working)
 2. Remove Food (Not working yet)
 3. Your Food (Not working yet)
 4. Change Quote (Working)
@@ -85,19 +86,41 @@ def food_dict_maker(csv_file):
 
     return food_dict
 
+def todays_date(food_file):
+    """This function overwrite the existing food file if the date has changed"""
+    today = datetime.today()
+    formatted_date = today.strftime("%Y, %d, %m")
+
+    try:
+        with open(food_file, "r") as daily_food_read:
+            first_line = daily_food_read.readline().strip()
+    except FileNotFoundError:
+        first_line = ""
+
+    if first_line != formatted_date:
+        with open(food_file, "w") as daily_food:
+            daily_food.write(f"{formatted_date}\n")
+    
+
+
 def add_food(food, food_file):
     """This function will allow the user to add a food to the 
-list on a txt file"""
-
+list of a txt file"""
     if food in food_dict:
-        calories = food_dict[food]
+        grams = int(input("What are the grams? "))
+        calories = float(food_dict[food])
     else:
-        calories = 0
+        calories = int(input("What are the calories per 100 grams? "))
+        grams = int(input("How many grams? "))
+    total_cals = (calories/100)*grams
     num_lines = 0
-    with open(food_file, "r") as daily_food_read:
-        num_lines = sum(1 for _ in daily_food_read) + 1
+    try:
+        with open(food_file, "r") as daily_food_read:
+            num_lines = sum(1 for _ in daily_food_read)
+    except FileNotFoundError:
+        num_lines = 1
     with open(food_file, "a") as daily_food:
-        daily_food.write(f"{num_lines}, {food}, {calories}\n")
+        daily_food.write(f"{num_lines}, {grams}, {food}, {total_cals}\n")
     return f"{food} was add to the daily list."
 
 
