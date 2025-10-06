1d75cd5d9ae41458a7eb3286cb5dfb7fc685230e
Baggins3345 <baggins3345@gmail.com>
2025-10-06 10:24:45 -0600

    started on add_food function

--- Week6/Final_assignment_MILESTONE/Calories.py
+++ Week6/Final_assignment_MILESTONE/Calories.py
@@ -44,7 +44,8 @@ Menu:
         elif users_choice == "1":
             food = input("What food would you like to add? ")
             fomatted_user = format_input(food)
-            add_food(fomatted_user,food_dict_csv)
+            print(add_food(fomatted_user,DAILY_FOOD))
+
         elif users_choice == "2":
             pass
         elif users_choice == "3":
@@ -87,7 +88,13 @@ def food_dict_maker(csv_file):
 def add_food(food, food_file):
     """This function will allow the user to add a food to the 
 list on a txt file"""
-    pass
+    num_lines = 0
+    with open(food_file, "r") as daily_food_read:
+        num_lines = sum(1 for _ in daily_food_read) + 1
+    with open(food_file, "a") as daily_food:
+        daily_food.write(f"{num_lines}: {food}\n")
+    return f"{food} was add to the daily list."
+            
 
 def remove_food(food, food_file):
     """This function takes the food prama and take it out of 
